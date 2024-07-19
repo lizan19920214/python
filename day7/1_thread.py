cabe7179ff15800e87d85fc34aa4b8227c0244e0
lizan19920214 <894295412@qq.com>
2024-07-19 16:54:00 +0800

    添加python线程描述

--- day7/1_thread.py
+++ day7/1_thread.py
@@ -1,3 +1,7 @@
+"""
+在 Python 中，threading 模块提供了多线程编程的基础。你可以通过创建 threading.Thread 对象来创建新的线程，
+并将目标函数和参数传递给线程。在创建和启动线程后，可以通过 join() 方法等待线程完成执行。
+"""
 from random import randint
 from threading import Thread
 from time import time, sleep
@@ -13,11 +17,15 @@ def download(filename):
 def main():
     start = time()
     t1 = Thread(target=download, args=('Python从入门到住院.pdf',))
-    t1.start()
     t2 = Thread(target=download, args=('vedio.avi',))
+
+    # 启动线程
+    t1.start()
     t2.start()
+    # 等待线程完成
     t1.join()
     t2.join()
+    
     end = time()
     print('总共耗费了%.3f秒' % (end - start))
 
